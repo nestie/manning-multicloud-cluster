dd15444f5a8561d7c20c4ef43e3b364b3b6f735d
Burkhard Nestmann <burkhard.nestmann@metaproc.com>
2021-06-21 13:42:16 +0200

    chapter 1.1

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "google" {
+  project = "manning-317419"
+  region  = "europe-west3"
+  zone    = "europe-west3-a"
+}
+
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "manning-gke-cluster"
+  location = "europe-west3"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "manning-node-pool"
+  location   = "europe-west3"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
